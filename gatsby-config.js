17455e7959f29b594bcbe63abc307dc99aa9468a
Jared Rooprai <hello@jaredrooprai.com>
2019-05-14 13:29:15 -0600

    corrected manifest img directory, added json parsing plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,8 +31,18 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#ffffff`,
         display: `minimal-ui`,
+        icon:`src/images/cru-hash.png`
       },
     },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
