3c528e26cd10bda1a2b46a0bfff062420bd0b501
Jared Rooprai <hello@jaredrooprai.com>
2019-09-05 12:04:34 -0600

    updated publications list to use react hooks

--- src/components/publicationslist.jsx
+++ src/components/publicationslist.jsx
@@ -1,83 +1,70 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Filter } from 'react-feather'
 
 import Padding from '../containers/padding'
 import Button from './button'
 import PublicationQuery from './publicationquery'
 
-class PublicationsList extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      activeList: 'all',
-    }
-  }
+const PublicationsList = () => {
+  const [activeList, setActiveList] = useState('all')
 
-  setList(x) {
-    this.setState({ activeList: x })
-  }
-
-  render() {
-    const { activeList } = this.state
-
-    const icon = () => {
-      return (
-        <>
-          <span> </span>
-          <Filter style={{ marginBottom: '-3px' }} size={15} color="grey" />
-        </>
-      )
-    }
-
-    const filterSelected = () => {
-      return '3px solid rgb(232, 56, 56)'
-    }
-
+  const icon = () => {
     return (
       <>
-        <Padding>
-          <div style={{ minHeight: '90vh' }}>
-            <Button
-              style={{
-                margin: 5,
-                width: 80,
-                border: activeList === 'all' && filterSelected(),
-              }}
-              onClick={() => this.setList('all')}
-            >
-              All
-            </Button>
-            <Button
-              style={{
-                width: 185,
-                margin: 5,
-                border: activeList === 'machineLearning' && filterSelected(),
-              }}
-              onClick={() => this.setList('machineLearning')}
-            >
-              Machine Learning
-              {icon()}
-            </Button>
-            <Button
-              style={{
-                width: 135,
-                margin: 5,
-                border: activeList === 'depression' && filterSelected(),
-              }}
-              onClick={() => this.setList('depression')}
-            >
-              Depression
-              {icon()}
-            </Button>
-            <br />
-            <br />
-            <br />
-            <PublicationQuery group={activeList} key={activeList} />
-          </div>
-        </Padding>
+        <span> </span>
+        <Filter style={{ marginBottom: '-3px' }} size={15} color="grey" />
       </>
     )
   }
+
+  const filterSelected = () => {
+    return '3px solid rgb(232, 56, 56)'
+  }
+
+  return (
+    <>
+      <Padding>
+        <div style={{ minHeight: '90vh' }}>
+          <Button
+            style={{
+              margin: 5,
+              width: 80,
+              border: activeList === 'all' && filterSelected(),
+            }}
+            onClick={() => setActiveList('all')}
+          >
+            All
+          </Button>
+          <Button
+            style={{
+              width: 185,
+              margin: 5,
+              border: activeList === 'machineLearning' && filterSelected(),
+            }}
+            onClick={() => setActiveList('machineLearning')}
+          >
+            Machine Learning
+            {icon()}
+          </Button>
+          <Button
+            style={{
+              width: 135,
+              margin: 5,
+              border: activeList === 'depression' && filterSelected(),
+            }}
+            onClick={() => setActiveList('depression')}
+          >
+            Depression
+            {icon()}
+          </Button>
+          <br />
+          <br />
+          <br />
+          <PublicationQuery group={activeList} key={activeList} />
+        </div>
+      </Padding>
+    </>
+  )
 }
 
 export default PublicationsList
