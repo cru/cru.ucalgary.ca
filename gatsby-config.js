6c88a0aa50349d55506aac47ec6ab36f1a40b2ca
Jared Rooprai <hello@jaredrooprai.com>
2019-11-08 14:23:37 -0700

    moved analytics to top of gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,35 @@ module.exports = {
     title: 'Clinical Research Unit',
     titleTemplate: 'CRU · %s',
     description: `Working with researchers in the Cumming School of Medicine to support the delivery of cutting edge research support services in the areas of data, technology and methodology.`,
-    url: 'https://cru-prototype.researchcalgary.ca/',
-    siteUrl: 'https://cru-prototype.researchcalgary.ca/',
+    url: 'https://cru.ucalgary.ca/',
+    siteUrl: 'https://cru.ucalgary.ca/',
     image: 'src/images/cru-hash.png',
     twitterUsername: '@crucalgary',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-125543132-3',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+        // Enables Google Optimize Experiment ID
+        experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
+        // Any additional optional fields
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -51,32 +74,6 @@ module.exports = {
       options: {
         host: 'https://cru-prototype.researchcalgary.ca/',
         policy: [{ userAgent: '*', allow: '/' }],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-125543132-3',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-        // Enables Google Optimize Experiment ID
-        experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'example.com',
       },
     },
   ],
